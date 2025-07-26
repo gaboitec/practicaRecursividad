7bcfe40ff305fb063820f9f4e7b0409f9df2c412
ALUMNO <caxajgabriel2@gmail.com>
2025-07-25 21:03:04 -0600

    menu principal + invertir cadena + calcular la suma de N numeros + cuenta regresiva

---
+++ main.py
@@ -0,0 +1,37 @@
+def invertir_cadena(cadena):
+    if len(cadena) == 0:
+        return ""
+
+    return cadena[-1] + invertir_cadena(cadena[:-1])
+
+def suma_n(numero):
+    if numero == 0:
+        return 0
+    return numero + suma_n(numero - 1)
+
+def cuenta(numero):
+    if numero == 0:
+        return 0
+    return f"{numero} {cuenta(numero - 1)}"
+
+while True:
+    print("\n==== MENU DE RECURSIVIDAD ====")
+    print("1. Invertir cadena de texto")
+    print("2. Calcular la suma de los primeros N numeros naturales")
+    print("3. Cuenta regresiva desde N hasta 1")
+    print("4. Sumar los digitos de un numero")
+    print("5. Salir")
+
+    opcion = input("Ingrese una opcion: ")
+
+    if opcion == "1":
+        cadena_ingreso = input("Ingrese una cadena de texto: ")
+        print(f"Resultado: {invertir_cadena(cadena_ingreso)}")
+
+    elif opcion == "2":
+        num = int(input("Ingrese un numero: "))
+        print(f"Resultado: {suma_n(num)}")
+
+    elif opcion == "3":
+        num = int(input("Ingrese un numero: "))
+        print(f"Resultado: {cuenta(num)}")
